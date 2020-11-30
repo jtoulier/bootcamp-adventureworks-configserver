19acdbe82073ebdf154ec6db5cb9d07b31cdddd9
jtoulier <jtoulier@gmail.com>
2020-11-30 16:07:54 -0500

    Initial commit

---
+++ src/main/java/com/bootcamp/adventureworks/configserver/ConfigServerDeAdventureWorksApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.adventureworks.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerDeAdventureWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerDeAdventureWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/adventureworks/configserver/ConfigServerDeAdventureWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.adventureworks.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDeAdventureWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
